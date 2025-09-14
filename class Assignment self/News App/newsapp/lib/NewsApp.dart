bd8800e93cc17886db05378418da68d810bd932b
Bhush2003 <bhushanbc2003@gmail.com>
2025-09-14 16:56:18 +0530

    Initial commit with all Flutter projects

---
+++ class Assignment self/News App/newsapp/lib/NewsApp.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewsApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return QuizAppState();
+  }
+}
+
+class QuizAppState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
